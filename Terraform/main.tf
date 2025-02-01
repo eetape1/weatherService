e12a53b2c6d6d46cef80e305a51d8f9c69526361
eetape1 <epumaetape@gmail.com>
2025-02-01 08:48:39 -0500

    Create main.tf
    starter main

---
+++ Terraform/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_eks_cluster" "existing_cluster" {
+  name = "WeatherApp"
+}
+
+data "aws_eks_cluster_auth" "existing_cluster" {
+  name = data.aws_eks_cluster.existing_cluster.name
+}
+
+resource "random_string" "node_group_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+  lower   = true
+  numeric   = true
+}
+
+resource "aws_eks_node_group" "eks_node_group" {
+  
+  cluster_name    = data.aws_eks_cluster.existing_cluster.name
+  node_group_name = "my-node-group-${random_string.node_group_suffix.result}"
+  node_role_arn   = "arn:aws:iam::381492235736:role/Amazon-EKS-Node-Group-Role"
+  
+  subnet_ids      = [
+    "subnet-0c5ca8131b25f1a9d",
+    "subnet-03b3a7aa9da4b81f4"
+  ]
+
+  # Private Subnets
+  # subnet_ids      = [
+  #  "subnet-0fbd27d62123de106",
+  #  "subnet-0d165e3cc8e537471"
+  # ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 2
+  }
+
+  ami_type       = "AL2_x86_64"
+  instance_types = ["t3.medium"]
+
+  tags = {
+    "eks:nodegroup-name" = "my-node-group-${random_string.node_group_suffix.result}"
+    "Name"               = "EKS-Node-Group"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
