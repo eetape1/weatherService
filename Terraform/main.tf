ef6a8af784ba8d3e8cca02e6e4b66080436f6ea2
eetape1 <epumaetape@gmail.com>
2025-02-02 11:58:40 -0500

    smaller node group - aws bill rising lol

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -45,8 +45,8 @@ resource "aws_eks_node_group" "eks_node_group" {
   # ]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 4
+    desired_size = 1
+    max_size     = 2
     min_size     = 1
   }
 
